86a541ef4848f6004aaff19954ed93d55fe5b4f9
keerimsk <sakizkerim@outlook.com>
2024-08-05 17:22:16 +0300

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Kerim Sakız on 5.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
